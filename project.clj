944017ab37ef14071605e6b3282242615261550a
François De Serres <justiniac@users.noreply.github.com>
2015-09-16 14:03:28 +0200

    Update project.clj
    gh link

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject net.cgrand/xforms "0.1.0"
   :description "Extra transducers for Clojure"
-  #_#_:url "http://example.com/FIXME"
+  :url "https://github.com/cgrand/xforms"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
