3bb82ad06d7ceab8085cb6cf22178d1287c89ef7
Christophe Grand <christophe@cgrand.net>
2017-04-04 09:37:03 +0200

    0.9.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.cgrand/xforms "0.9.2"
+(defproject net.cgrand/xforms "0.9.3"
   :description "Extra transducers for Clojure"
   :url "https://github.com/cgrand/xforms"
   :license {:name "Eclipse Public License"
