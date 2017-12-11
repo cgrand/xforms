50d6611bfbe0cd2c336a45a94ea8e7630bd7038a
Christopher Mark Gore <chris.gore@climate.com>
2017-12-11 11:08:54 -0600

    Clojure 1.9

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/cgrand/xforms"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946"]
                  [net.cgrand/macrovich "0.2.0"]])
