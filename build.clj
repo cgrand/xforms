78076f8cd078ebb336ed9047df873ebc2ecf3aa1
Imre Kószó <imrekoszo@users.noreply.github.com>
2024-01-22 13:15:10 +0100

    update build script
    - add scm block to pom so it links to the repo
    - add license as it is now required by clojars
    - fix paths in build alias
    - bump tools.build

--- build.clj
+++ build.clj
@@ -7,6 +7,19 @@
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def scm {:connection "scm:git:git://github.com/cgrand/xforms.git"
+          :developerConnection "scm:git:git://github.com/cgrand/xforms.git"
+          :url "https://github.com/cgrand/xforms"})
+(def extra-pom-data
+  [[:licenses
+    [:license
+     [:name "Eclipse Public License 1.0"]
+     [:url "https://opensource.org/license/epl-1-0/"]
+     [:distribution "repo"]]
+    [:license
+     [:name "Eclipse Public License 2.0"]
+     [:url "https://opensource.org/license/epl-2-0/"]
+     [:distribution "repo"]]]])
 
 (defn clean [_]
   (b/delete {:path "target"}))
@@ -16,7 +29,9 @@
                 :lib lib
                 :version version
                 :basis basis
-                :src-dirs ["src"]})
+                :src-dirs ["src"]
+                :scm (assoc scm :tag (str "v" version))
+                :pom-data extra-pom-data})
   (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
